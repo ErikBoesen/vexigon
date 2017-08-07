d580294a42fbf1839d9c6d3ef24bac5e7d185640
ErikBoesen <me@erikboesen.com>
2017-08-07 18:55:00 -0400

    Moved all content onto one page

--- scripts.js
+++ scripts.js
@@ -19,3 +19,12 @@ $(function() {
         }
     });
 });
+
+$('nav ul a').on('click', function() {
+    $.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: '#' + this.id
+    });
+
+    return false;
+});
