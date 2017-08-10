d3d8437cecdc31c178e4ffe2c5044ad337d1d73a
ErikBoesen <me@erikboesen.com>
2017-08-10 13:14:47 -0400

    Background color is gradient behind separate triangle element

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 var COLORS = [
-    '#28beff',
-    '#ff8024',
-    '#2453a2'
+    ['#28beff', '#30c1ff'],
+    ['#f15a3d', '#f7901e'],
+    ['#2453a2', '#255dba']
 ];
 
 function colorize(num) {
     console.log(num);
-    document.body.style.background = 'linear-gradient(to bottom right, ' + COLORS[num - 1] + ' 50%, #ddd 50%)';
+    document.body.style.background = 'linear-gradient(to top right, ' + COLORS[num - 1][0] + ', ' + COLORS[num - 1][1] + ')';
 }
 
 $(function() {
@@ -21,10 +21,9 @@ $(function() {
 });
 
 $('nav ul a').on('click', function() {
+    var target = this.href.split('#')[1];
     $.smoothScroll({
         scrollElement: $('body'),
-        scrollTarget: '#' + this.id
+        scrollTarget: '#' + target
     });
-
-    return false;
 });
