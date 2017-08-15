040205f7a852c9b773e9b73c026675b2c0406b75
ErikBoesen <me@erikboesen.com>
2017-08-14 22:59:20 -0400

    Replace SlidesJS with Slick, integrate three pages better, responsiveness

--- scripts.js
+++ scripts.js
@@ -4,21 +4,25 @@ var COLORS = [
     ['#2453a2', '#255dba']
 ];
 
-function colorize(num) {
+function colorize(event, slick, previous, num) {
     console.log(num);
-    document.body.style.background = 'linear-gradient(to top right, ' + COLORS[num - 1][0] + ', ' + COLORS[num - 1][1] + ')';
+    document.body.style.background = 'linear-gradient(to top right, ' + COLORS[num][0] + ', ' + COLORS[num][1] + ')';
 }
 
 $(function() {
-    $("#slides").slidesjs({
-        width: 800,
-        height: window.innerWidth >= 700 ? 650 : window.innerHeight * 1.15,
-        callback: {
+    $("#slides").slick({
+        autoplay: true,
+        arrows: false,
+        dots: true
+        /*callback: {
             start: colorize,
             loaded: colorize
-        }
+        }*/
     });
 });
+
+$('#slides').on('load', colorize);
+$('#slides').on('beforeChange', colorize);
 
 $('nav ul a').on('click', function() {
     var target = this.href.split('#')[1];
