c818e570a98a4b02b82f77dd5fe7dece90ff1605
ErikBoesen <me@erikboesen.com>
2017-08-16 13:18:54 -0400

    Scrolling works right

--- scripts.js
+++ scripts.js
@@ -1,12 +1,24 @@
+var bg = document.getElementsByClassName('bg-triangle')[0];
+var navItems = document.querySelectorAll('nav li');
+var header = document.getElementsByTagName('header')[0];
+
+document.body.onscroll = function() {
+    bg.style.transform = 'translateX(' + document.body.scrollTop*0.8 + 'px)';
+
+    for (i = 0; i < navItems.length; i++) navItems[i].id = '';
+
+    navItems[Math.round(document.body.scrollTop / (window.innerHeight-65))].id = 'selected';
+};
+
 var COLORS = [
-    ['#28beff', '#30c1ff'],
     ['#f15a3d', '#f7901e'],
+    ['#28beff', '#30c1ff'],
     ['#2453a2', '#255dba']
 ];
 
 function colorize(event, slick, previous, num) {
-    console.log(num);
-    document.body.style.background = 'linear-gradient(to top right, ' + COLORS[num][0] + ', ' + COLORS[num][1] + ')';
+    document.body.style.background = 'linear-gradient(50deg, ' + COLORS[num][0] + ' 30%, ' + COLORS[num][1] + ')';
+    header.style.background = COLORS[num][1];
 }
 
 $(function() {
@@ -14,20 +26,19 @@ $(function() {
         autoplay: true,
         arrows: false,
         dots: true
-        /*callback: {
-            start: colorize,
-            loaded: colorize
-        }*/
     });
 });
 
-$('#slides').on('load', colorize);
 $('#slides').on('beforeChange', colorize);
 
-$('nav ul a').on('click', function() {
-    var target = this.href.split('#')[1];
+$('nav ul a').on('click', function(e) {
+    e.preventDefault();
+    var hash = this.hash;
     $.smoothScroll({
         scrollElement: $('body'),
-        scrollTarget: '#' + target
+        scrollTarget: hash,
+        afterScroll: function() {
+            window.location.hash = hash;
+        }
     });
 });
