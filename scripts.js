ad8f01754acfd35cac663ace863cd15df0141724
ErikBoesen <me@erikboesen.com>
2017-10-03 13:11:06 -0400

    Some mobile fixes and stylistic tweaks

--- scripts.js
+++ scripts.js
@@ -1,15 +1,25 @@
 var navItems = document.querySelectorAll('nav li');
+var sections = document.getElementsByTagName('article')
 var header = document.getElementsByTagName('header')[0];
 var headerBG = document.getElementsByClassName('header-bg')[0];
-var spotlight = document.getElementById('spotlight');
-
+var spotlight = document.getElementById('spotlight-section');
 
 document.body.onscroll = function() {
+    var top = document.documentElement.scrollTop;
     for (i = 0; i < navItems.length; i++) navItems[i].id = '';
 
-    navItems[Math.round(document.body.scrollTop / (window.innerHeight-65))].id = 'selected';
+    var dist;
+    var closest;
+    for (i = 0; i < sections.length; i++) {
+        var gap = Math.abs(top - sections[i].offsetTop);
+        if (dist === undefined || gap < dist) {
+            closest = i;
+            dist = gap;
+        }
+    }
+    navItems[closest].id = 'selected';
 
-    headerBG.style.opacity = (document.body.scrollTop > 200) ? 1 : 0;
+    headerBG.style.opacity = (top > 200) ? 1 : 0;
 };
 
 var COLORS = [
@@ -41,7 +51,9 @@ $('nav ul a').on('click', function(e) {
     });
 });
 
-$.scrollify({
-	section: 'article',
-    scrollSpeed: 400
-});
+if (document.body.clientWidth > 800 && false) {
+    $.scrollify({
+        section: 'article',
+        scrollSpeed: 400
+    });
+}
